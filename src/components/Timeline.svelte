c504c89a373bdeca22c8a42c68a294f7f34b95fd
Jakub Zachacz <zachacz.jakub@gmail.com>
2020-03-09 13:10:44 +0100

    Timeline WIP

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,11 +1,18 @@
 <script>
   import Item from "./Item.svelte";
   import { portfolioItems } from "../portfolio/items";
-
-  console.log(portfolioItems);
+  let counter = 0;
+  const renderItems = [];
+  for (let { year, items } of portfolioItems) {
+    renderItems.push(year);
+    for (let item of items) {
+      renderItems.push({ ...item, index: counter });
+      counter++;
+    }
+  }
 </script>
 
-<style>
+<style lang="scss">
   .timeline-lines {
     position: fixed;
     top: 0;
@@ -45,29 +52,40 @@
       rgba(255, 97, 150, 1) 100%
     );
   }
-  /* .item {
-    position: relative;
-    padding: 100px 0;
-    text-align: center;
-    z-index: -15;
-  } */
+  .year-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    .year {
+      background: white;
+      padding: 4px 16px;
+      font-weight: 600;
+      font-size: 1.4rem;
+      border-bottom: 2px solid #537df0;
+      border-top: 2px solid #f71360;
+    }
+  }
 </style>
 
-<!-- <div id="timeline-lines" class="timeline-lines"> -->
 <div id="timeline-lines" class="timeline-lines unrevealed">
   <div class="line technology" />
   <div class="line creativity" />
 </div>
-<div style="height: 200px" />
-{#each portfolioItems as item}
-  <Item {...item} />
+<div />
+<div id="timeline" style="height: 200px" />
+{#each renderItems as item}
+  {#if typeof item === 'number'}
+    <div class="year-wrapper">
+      <div class="year">&#8226;{item}&#8226;</div>
+      <!-- <div class="year">&#x2728;{item}&#x2728;</div> -->
+    </div>
+  {:else}
+    <Item {...item} />
+  {/if}
 {/each}
-<!-- <div class="item">
-  <p>PIERWSZY ELEMENT</p>
-</div>
-<div class="item">
-  <p>DRUGI ELEMENT</p>
-</div>
-<div class="item">
-  <p>CZECI ELEMENT</p>
-</div> -->
+<!-- {#each portfolioItems as yearGroup}
+  <div>{yearGroup.year}</div>
+  {#each yearGroup.items as item, i}
+    <Item {...item} />
+  {/each}
+{/each} -->
--- src/portfolio/items.js
+++ src/portfolio/items.js
@@ -25,36 +25,54 @@ const tools = {
 
 export const portfolioItems = [
   {
-    id: "portfolio-page",
-    srcType: src.img,
-    minSrc: "portfolio.png",
-    // minSrc: "placeholder.jpg",
-    src: "portfolio.png",
-    tags: [tags.prv, tags.graphic, tags.front],
-    tools: [tools.svelte, tools.illustrator]
+    year: 2020,
+    items: [
+      {
+        id: "portfolio-page",
+        srcType: src.img,
+        minSrc: "portfolio.png",
+        // minSrc: "placeholder.jpg",
+        src: "portfolio.png",
+        tags: [tags.prv, tags.graphic, tags.front],
+        tools: [tools.svelte, tools.illustrator]
+      },
+      {
+        id: "bastion-robot",
+        srcType: src.video,
+        minSrc: "placeholder.jpg",
+        src: "https://www.youtube.com/embed/TWDAUc1G264",
+        tags: [tags.prv, tags.engineering],
+        tools: [tools.inventor, tools.arduino]
+      },
+      {
+        id: "placeholder",
+        srcType: src.img,
+        minSrc: "placeholder.jpg",
+        src: "placeholder.jpg",
+        tags: [tags.prv, tags.engineering],
+        tools: [tools.inventor, tools.arduino]
+      },
+    ]
   },
   {
-    id: "bastion-robot",
-    srcType: src.video,
-    minSrc: "placeholder.jpg",
-    src: "https://www.youtube.com/embed/TWDAUc1G264",
-    tags: [tags.prv, tags.engineering],
-    tools: [tools.inventor, tools.arduino]
-  },
-  {
-    id: "placeholder",
-    srcType: src.img,
-    minSrc: "placeholder.jpg",
-    src: "placeholder.jpg",
-    tags: [tags.prv, tags.engineering],
-    tools: [tools.inventor, tools.arduino]
-  },
-  {
-    id: "placeholder",
-    srcType: src.img,
-    minSrc: "placeholder.jpg",
-    src: "placeholder.jpg",
-    tags: [tags.prv, tags.engineering],
-    tools: [tools.inventor, tools.arduino]
+    year: 2019,
+    items: [
+      {
+        id: "placeholder",
+        srcType: src.img,
+        minSrc: "placeholder.jpg",
+        src: "placeholder.jpg",
+        tags: [tags.prv, tags.engineering],
+        tools: [tools.inventor, tools.arduino, tools.illustrator, tools.material, tools.redux]
+      },
+      {
+        id: "placeholder",
+        srcType: src.img,
+        minSrc: "placeholder.jpg",
+        src: "placeholder.jpg",
+        tags: [tags.prv, tags.engineering],
+        tools: [tools.inventor, tools.arduino]
+      }
+    ]
   }
 ];
